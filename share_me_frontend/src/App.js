6a5152c23a3e35ffe4950710565e3cc868a152fe
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-12-04 10:24:56 +0530

    added image

--- share_me_frontend/src/App.js
+++ share_me_frontend/src/App.js
@@ -1,38 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { userQuery } from "./utiles/data";
+
 import { fetchUser } from "./utiles/fetchUser";
 
-import { client } from "./client";
-
 const App = () => {
-  const [user, setUser] = useState(null);
-  const userInfo = fetchUser();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const query = userQuery(userInfo?._id);
-    client.fetch(query).then((data) => setUser(data[0]));
-    console.log(user);
-  }, [user, userInfo?._id]);
+    const user = fetchUser();
+
+    if (!user) return navigate("/login");
+  }, []);
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
       <Routes>
-        {!user?._id ? (
-          <>
-            {" "}
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<Login />} />
-          </>
-        ) : (
-          <>
-            <Route path="login" element={<Login />} />
-            <Route path="/*" element={<Home />} />
-          </>
-        )}
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </GoogleOAuthProvider>
   );
