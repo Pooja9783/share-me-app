54f5517d7033fd882557f6e333256f3a6b3a923f
Pooja9783 <poojasankhala97@gmail.com>
2023-06-07 23:30:07 +0530

    Removed un-neccessary files

--- share_me_frontend/src/components/Feed.jsx
+++ share_me_frontend/src/components/Feed.jsx
@@ -8,7 +8,6 @@ const Feed = () => {
   const [loading, setLoading] = useState(false);
   const [pins, setPins] = useState(null);
   const { categoryId } = useParams();
-  // console.log(categoryId);
   useEffect(() => {
     setLoading(true);
     if (categoryId) {
